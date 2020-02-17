4db38bd1ccd9f92e5fe28ad72197ce989475241d
DELL <DELL@DELL-PC>
2020-02-17 11:49:17 +0530

    create m1 method

--- GitAmit/src/test/Testt1.java
+++ GitAmit/src/test/Testt1.java
@@ -2,10 +2,18 @@ package test;
 
 public class Testt1 
 {
+	
+	public void m1()
+	{
+		System.out.println("new update chingae");
+	}
 	public static void main(String[] args) {
 		System.out.println("hello");
 		Testt1 t=new Testt1();
-             System.out.println("new edit");
+        System.out.println("new edit");
+             
+             
+             
 	}
 
 }
