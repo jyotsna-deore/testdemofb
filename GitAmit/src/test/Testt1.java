c9428ed569b9aa2bf11d52524b02518b18b1c7d0
jyotsna-deore <61134285+jyotsna-deore@users.noreply.github.com>
2020-02-17 11:50:45 +0530

    Update Testt1.java

--- GitAmit/src/test/Testt1.java
+++ GitAmit/src/test/Testt1.java
@@ -10,7 +10,10 @@ public class Testt1
 	public static void main(String[] args) {
 		System.out.println("hello");
 		Testt1 t=new Testt1();
+		
+		t.m1();
         System.out.println("new edit");
+		
              
              
              
